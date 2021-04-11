3d043047ace4f21553e75d2a2ec987a32b5fd941
Reza Ageng <rezaageng5@gmail.com>
2021-04-11 20:40:41 +0700

    namespace

--- p13autoloading/index.php
+++ p13autoloading/index.php
@@ -1,18 +1,11 @@
 <?php
 require_once 'App/init.php';
 
-// $produk1 = new Komik("Jujutsu Kaisen", "Gege Akutami", "Elex", 40000, 200);
-// $produk2 = new Game("Little Nighmares", "Trasier Studios", "Bandai Namco", 239000, 100);
+$produk1 = new Komik("Jujutsu Kaisen", "Gege Akutami", "Elex", 40000, 200);
+$produk2 = new Game("Little Nighmares", "Trasier Studios", "Bandai Namco", 239000, 100);
 
-// $cetakProduk = new CetakInfoProduk();
-// $cetakProduk->tambahProduk($produk1);
-// $cetakProduk->tambahProduk($produk2);
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
 
-// echo $cetakProduk->cetak();
-
-use App\Produk\User as ProdukUser;
-use App\Service\User as ServiceUser;
-
-new ProdukUser();
-echo '<br>';
-new ServiceUser();
+echo $cetakProduk->cetak();
